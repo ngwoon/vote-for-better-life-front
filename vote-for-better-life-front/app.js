33123da391cefe604b8ab6f4eba061eb54d57270
ngwoon <todory2002@gmail.com>
2020-12-05 18:19:24 +0900

    1차 완성

--- vote-for-better-life-front/app.js
+++ vote-for-better-life-front/app.js
@@ -1,11 +1,11 @@
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -20,11 +20,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    res.render("error");
 });
 
 // error handler
